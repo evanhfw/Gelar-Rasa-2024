cc2e256e3901df8196325cd7adfdd48d1477a4af
Evan Hanif Widiatama <121467356+evanhfw@users.noreply.github.com>
2024-11-16 21:45:40 +0700

    Update streamlit_apps.py

--- streamlit/streamlit_apps.py
+++ streamlit/streamlit_apps.py
@@ -5,11 +5,11 @@ import joblib
 import numpy as np
 
 # Load model dan kategori yang digunakan pada data pelatihan
-model = xgb.Booster(model_file='../Classification/fraud_detection.json')
-category_categories = list(set(joblib.load('../joblib/category.joblib')))  # Pastikan unik
-city_categories = list(set(joblib.load('../joblib/city.joblib')))          # Pastikan unik
-job_categories = list(set(joblib.load('../joblib/job.joblib')))            # Pastikan unik
-X_train = joblib.load('../joblib/X_train.pkl')
+model = xgb.Booster(model_file='fraud_detection.json')
+category_categories = list(set(joblib.load('category.joblib')))  # Pastikan unik
+city_categories = list(set(joblib.load('city.joblib')))          # Pastikan unik
+job_categories = list(set(joblib.load('job.joblib')))            # Pastikan unik
+X_train = joblib.load('X_train.pkl')
 
 def get_input():
     amt = st.number_input('Jumlah Transaksi', min_value=0.0, value=0.0)
